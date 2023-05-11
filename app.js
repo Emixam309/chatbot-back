d36fc91db36bcb7ca30496b760d5979578f31498
Maxime Croisier <crmaxime@gmail.com>
2023-05-11 12:29:15 +0200

    add MVC

--- app.js
+++ app.js
@@ -2,21 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const dialogs = require('./dialogs.json')
+app.use('/api/v1', require('./routes/v1'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/dialog/questions', (req, res) => {
-  const questions = dialogs.map(({id, question}) => ({id, question}))
-  res.json(questions)
-})
-
-app.get('/dialog/answer/:id', (req, res) => {
-  const dialog = dialogs.find(dialog => dialog.id === parseInt(req.params.id))
-  if (!dialog) return res.status(404).send('Dialog not found')
-  res.json(dialog)
+app.get('*', (req, res) => {
+  // res.status(404).json({message: 'Not found'}) bonne pratique
+  res.sendFile(__dirname + '/view/404.html')
 })
 
 app.listen(port, () => {
